5bb0188a2c4e697eeb040fa39f8d3975902deebb
Antonio Gabriel <antonio.gabrielTI@outlook.com>
2023-03-22 23:29:39 -0300

    ADD -> Landing page - model 1

---
+++ LANDING PAGE - MODEL 1/SCRIPTS/script.js
@@ -0,0 +1,6 @@
+const toggleButton = document.querySelector(".menu-toggle input");
+const menu = document.querySelector(".menu-toggle ul");
+
+toggleButton.addEventListener("click", function () {
+  menu.classList.toggle("active");
+});
